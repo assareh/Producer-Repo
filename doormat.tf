4536eb4f3149546677bc597eb098eab58e256a0e
assareh <assareh@hashicorp.com>
2022-07-21 10:20:45 -0700

    updating doormat provider

--- doormat.tf
+++ doormat.tf
@@ -1,5 +1,10 @@
+variable "TFC_WORKSPACE_NAME" {
+  type    = string
+  default = "" # An error occurs when you are running TF backend other than Terraform Cloud
+}
+
 data "tfe_outputs" "doormat_role" {
-  organization = var.org
+  organization = "hashidemos"
   workspace    = "doormat-aws-infra"
 }
 
@@ -8,5 +13,14 @@ provider "doormat" {}
 data "doormat_aws_credentials" "creds" {
   provider = doormat
 
-  role_arn = data.tfe_outputs.doormat_role.values.terraform_role
+  role_arn = "${data.tfe_outputs.doormat_role.values.role_arn_base}${var.TFC_WORKSPACE_NAME}"
+}
+
+terraform {
+  required_providers {
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
+    }
+  }
 }
